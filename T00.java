7554f01b770a6f364f98d028ed34ada478e575a1
Ignacia Putri Manurung <ignaciaputrimanurung@gmail.com>
2025-10-03 15:33:57 +0700

    PAIR 24
    Sudah di perbaiki

--- T00.java
+++ T00.java
@@ -5,44 +5,42 @@ public class T00 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int bil1, bil2, total, tambah, kali;
+        int a, b, sisa;
 
-        System.out.println("Input bilangan 1 : ");
-        bil1 = Integer.parseInt(input.nextLine());
-        if (bil1 % 2 == 0) {
+        a = Integer.parseInt(input.nextLine());
+        b = Integer.parseInt(input.nextLine());
+        sisa = a % 2;
+        if (sisa == 0) {
             System.out.println("Bilangan pertama: Genap");
         } else {
             System.out.println("Bilangan pertama: Ganjil");
         }
-        System.out.println("Input bilangan 2 : ");
-        bil2 = Integer.parseInt(input.nextLine());
-        if (bil2 % 2 == 0) {
-            System.out.println("Bilangan pertama: Genap");
+        sisa = b % 2;
+        if (sisa == 0) {
+            System.out.println("Bilangan kedua : Genap");
         } else {
-            System.out.println("Bilangan pertama: Ganjil");
+            System.out.println("Bilangan kedua: Ganjil");
         }
-        if (bil1 > bil2) {
-            if (bil1 == bil2) {
-                System.out.println("Kedua bilangan sama besar");
-            } else {
+        if (a == b) {
+            System.out.println("kedua bilangan sama besar");
+        } else {
+            if (a > b) {
                 System.out.println("Bilangan pertama lebih besar");
-            }
-        } else {
-            System.out.println("Bilangan kedua lebih besar");
-        }
-        if (bil1 % 2 == 0 && bil2 % 2 == 0) {
-            tambah = bil1 + bil2;
-            System.out.println("Hasil penjumlahan: " + tambah);
-        } else {
-            if (bil1 % 2 != 0 && bil2 % 2 != 0) {
-                kali = bil1 * bil2;
-                System.out.println("Hasil perkalian: " + kali);
             } else {
-                if (bil1 % 2 == 0 && bil2 % 2 != 0) {
-                    System.out.println("Kedua bilangan sama besar");
+                System.out.println("Bilangan kedua lebih besar");
+            }
+        }
+        if (a % 2 == 0 && b % 2 == 0) {
+            System.out.println(" Hasil penjumlahan: " + a + b);
+        } else {
+            if (a % 2 != 0 && b % 2 != 0) {
+                System.out.println(" Hasil perkalian: " + a * b);
+            } else {
+                if (a % 2 != 0 && b % 2 == 0) {
+                    System.out.println("Berbeda Jenis");
                 } else {
-                    if (bil1 % 2 != 0 && bil2 % 2 == 0) {
-                        System.out.println("Kedua bilangan sama besar");
+                    if (a % 2 == 0 && b % 2 != 0) {
+                        System.out.println("Berbeda Jenis");
                     }
                 }
             }
